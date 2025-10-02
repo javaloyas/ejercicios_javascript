5da6cd0315f80b3fbc7b6b15766fd9ceac37746e
Manuel Silva <tu_correo_de_github@example.comuser.mail=javaloyas@gmail.com>
2025-10-02 06:13:34 +0200

    Añadido ejercicio 10 (media volumen con for...of y reduce)

---
+++ ejercicio_10/script.js
@@ -0,0 +1,50 @@
+// Ejercicio 10
+const users = [
+  {
+    name: "Alberto",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Antonio",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Pedro",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Cristina",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+
+// Recorremos con for...of (usuarios) y for...in (cada sonido)
+let totalVolume = 0;
+let count = 0;
+
+for (const user of users) {
+  for (const sound in user.favoritesSounds) {
+    totalVolume += user.favoritesSounds[sound].volume;
+    count++;
+  }
+}
+
+const averageVolume = totalVolume / count;
+
+console.log("10. Media del volumen de todos los sonidos:", averageVolume);
